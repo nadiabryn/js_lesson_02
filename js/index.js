28573bf08c40afaa14e74031d9c9381975fd0cc5
nadiabryn <nadya.bryn@gmail.com>
2019-09-02 16:04:48 +0000

    js exumple

---
+++ js/index.js
@@ -0,0 +1,47 @@
+/*for (let i = 100; i >= 1; i--) {
+    if ( i % 2 === 0 && i % 10 !== 0) {
+        document.write(i + '<br>');
+    }
+
+}*/
+
+// let i = 0;
+// while (i<100) {
+//     document.write (i + '<br>');
+//     i++;
+// }
+
+// let i = 0;
+// do {
+//     document.write (i + '<br>');
+//     i++;
+
+// } while (i<100);
+
+
+// let num = parseFloat(prompt('Enter number'));
+// while (Number.isNan(num)){
+//     alert ('You entered not a number');
+//     num = parseFloat(prompt('Enter number'));
+// }
+
+// let products = ['Tesla X', 'Tesla S', 'BMW i3'];
+// products[3] = 'Tesla Y';
+// products.push('Chevy Corvette');
+// console.log(products.length);
+// products[products.length] = 'Audi';
+// products.push('Mercedes');
+// console.log(products.length);
+
+// for (let i = 0; i < products.length; i++){
+//     if (products[i] !== undefine) {
+//          console.log(products[i]);
+//     }
+// }
+
+// products.forEach(function(el){
+//     console.log(el);
+//     });
+
+// products[10] = 'jd';
+// products.forEach( el => console.log(el));
